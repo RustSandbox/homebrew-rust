582ca243501dc69a1ef9a67f91d8d74d4ea5d4e6
Hamze GHALEBI <ghalebi@gmail.com>
2025-05-28 14:48:19 +0200

    Fix SHA256 hash for v0.1.0 release

--- Formula/rust-new.rb
+++ Formula/rust-new.rb
@@ -2,7 +2,7 @@ class RustNew < Formula
   desc "Scaffold a Rust project from Hamze's starter template"
   homepage "https://github.com/RustSandbox/rust-new"
   url "https://github.com/RustSandbox/rust-new/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "d61e2b12f3c6ff70e8f8aad644180bc08a3b3540ac9394c49ef4f32c60547692"
   license "MIT"
 
   def install
