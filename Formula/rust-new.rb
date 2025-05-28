1b9bad31c2dc351e15f275812753e58a686ee3c6
Hamze GHALEBI <ghalebi@gmail.com>
2025-05-28 15:46:23 +0200

    Update to v0.2.0 with hghalebi/rust_template

--- Formula/rust-new.rb
+++ Formula/rust-new.rb
@@ -1,8 +1,8 @@
 class RustNew < Formula
   desc "Scaffold a Rust project from Hamze's starter template"
   homepage "https://github.com/RustSandbox/rust-new"
-  url "https://github.com/RustSandbox/rust-new/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "d61e2b12f3c6ff70e8f8aad644180bc08a3b3540ac9394c49ef4f32c60547692"
+  url "https://github.com/RustSandbox/rust-new/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "5bfe194fb6f05d279747a729863f9200749c0b4b1e31f0a4fc032f7c9d6b4a77"
   license "MIT"
 
   def install
